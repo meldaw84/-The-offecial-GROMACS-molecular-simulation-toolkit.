9b9719cb3933e44319372f27e008fdd6a48759e2
Mark Abraham <mark.j.abraham@gmail.com>
2021-10-26 13:12:59 +0200

    Use conditional compilation to avoid clang-analyzer problem

--- src/external/googletest/googletest/include/gtest/gtest.h
+++ src/external/googletest/googletest/include/gtest/gtest.h
@@ -2468,13 +2468,17 @@ TestInfo* RegisterTest(const char* test_suite_name, const char* test_name,
     Factory factory_;
   };
 
-  //NOLINTNEXTLINE(clang-analyzer-cplusplus.NewDeleteLeaks)
+#ifndef __clang_analyzer__
   return internal::MakeAndRegisterTestInfo(
       test_suite_name, test_name, type_param, value_param,
       internal::CodeLocation(file, line), internal::GetTypeId<TestT>(),
       internal::SuiteApiResolver<TestT>::GetSetUpCaseOrSuite(file, line),
       internal::SuiteApiResolver<TestT>::GetTearDownCaseOrSuite(file, line),
       new FactoryImpl{std::move(factory)});
+#else
+  // Works because this path is only taken during static analysis
+  return nullptr;
+#endif
 }
 
 }  // namespace testing
