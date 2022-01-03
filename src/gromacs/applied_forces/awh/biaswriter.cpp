ebf1265fad4f29659a1fee1e0cde59d751a96fa8
Dmitry Morozov <aracsmd@gmail.com>
2022-01-03 11:15:10 +0200

    Unwrap Dihedral coordinates in AWH output

--- src/gromacs/applied_forces/awh/biaswriter.cpp
+++ src/gromacs/applied_forces/awh/biaswriter.cpp
@@ -1,7 +1,8 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017,2018,2019,2020,2021, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018,2019 by the GROMACS development team.
+ * Copyright (c) 2020,2021,2022, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -205,6 +206,19 @@ static void normalizeBlock(AwhEnergyBlock* block, const Bias& bias)
             for (float& point : data)
             {
                 point *= block->normalizationValue;
+            }
+            /* Check if coordinate require unwarping by 360 deg
+               in that case the first element will be bigger than the last element */
+            if (data.front() > data.back())
+            {
+                const float period = 360;
+                for (float& point : data)
+                {
+                    if (point > data.back())
+                    {
+                        point -= period;
+                    }
+                }
             }
             break;
         case Normalization::FreeEnergy:
