4159aa86f0bd01d50dab9ada7aea93ddc3853739
Artem Zhmurov <zhmurov@gmail.com>
2022-01-13 14:11:06 +0300

    Do not use ObservablesReducer in constraints if running in one rank
    The ObservablesReducers is only needed when there are values to reduce,
    which is not the case when there is only one rank.
    
    Refs. #4331

--- src/gromacs/mdrun/runner.cpp
+++ src/gromacs/mdrun/runner.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2011-2019,2020,2021, by the GROMACS development team, led by
+ * Copyright (c) 2011-2019,2020,2021,2022, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1980,7 +1980,7 @@ int Mdrunner::mdrunner()
                                       &nrnb,
                                       wcycle.get(),
                                       fr->bMolPBC,
-                                      &observablesReducerBuilder);
+                                      PAR(cr) ? &observablesReducerBuilder : nullptr);
 
         /* Energy terms and groups */
         gmx_enerdata_t enerd(mtop.groups.groups[SimulationAtomGroupType::EnergyOutput].size(),
