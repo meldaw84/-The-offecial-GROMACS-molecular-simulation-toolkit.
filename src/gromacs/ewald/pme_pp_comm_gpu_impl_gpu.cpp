1316343388eee8c4f55979da5f817e89bf0bfc62
Andrey Alekseenko <al42and@gmail.com>
2022-08-09 22:55:26 +0200

    Cast buffer to ptr
    This was a really unobvious crash...

--- src/gromacs/ewald/pme_pp_comm_gpu_impl_gpu.cpp
+++ src/gromacs/ewald/pme_pp_comm_gpu_impl_gpu.cpp
@@ -149,7 +149,7 @@ void PmePpCommGpu::Impl::receiveForceFromPmeGpuAwareMpi(Float3* pmeForcePtr, int
     else
     {
         // Receive data from remote GPU in memory of local GPU
-        MPI_Recv(d_pmeForces_, recvSize * DIM, MPI_FLOAT, pmeRank_, 0, comm_, MPI_STATUS_IGNORE);
+        MPI_Recv(asMpiPointer(d_pmeForces_), recvSize * DIM, MPI_FLOAT, pmeRank_, 0, comm_, MPI_STATUS_IGNORE);
         if (pmeForcePtr != asMpiPointer(d_pmeForces_)) // destination is CPU memory, so finalize transfer with local D2H
         {
             copyFromDeviceBuffer(reinterpret_cast<RVec*>(pmeForcePtr),
