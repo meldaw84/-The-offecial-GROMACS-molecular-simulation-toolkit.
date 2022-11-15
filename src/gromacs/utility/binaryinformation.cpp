c81e393037568b9d67b8fd49f6f056f48ac16405
Andrey Alekseenko <al42and@gmail.com>
2022-11-15 18:40:05 +0100

    Add a comment and an assert

--- src/gromacs/utility/binaryinformation.cpp
+++ src/gromacs/utility/binaryinformation.cpp
@@ -273,6 +273,10 @@ std::string getGpuFftDescriptionString()
         }
         else
         {
+            /* Some SYCL builds (e.g., Intel DPC++ for AMD devices) have no support for GPU FFT,
+             * but that's a corner case not intended for general users */
+            GMX_RELEASE_ASSERT(GMX_GPU_SYCL,
+                               "Only SYCL build can function without a GPU FFT library");
             return "none / unknown";
         }
     }
