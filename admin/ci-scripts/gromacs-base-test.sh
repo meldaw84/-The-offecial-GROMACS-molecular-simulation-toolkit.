d69e338054944b0660f8574cee47718a8cca054b
Alan Gray <alangray3@gmail.com>
2022-12-02 14:23:41 +0000

    addressed comments

--- admin/ci-scripts/gromacs-base-test.sh
+++ admin/ci-scripts/gromacs-base-test.sh
@@ -17,7 +17,7 @@ fi
 if grep -qF 'nvidia.com/gpu' <<< "$KUBERNETES_EXTENDED_RESOURCE_NAME"; then
     nvidia-smi -L && nvidia-smi || true;
     computeCapability=`nvidia-smi -i 0 --query-gpu=compute_cap  --format=csv | tail -1 | sed 's/\.//g' `
-    if [ "$GMX_GPU_PME_DECOMPOSITION_CUFFTMP" != "" ] && [ "$computeCapability" -lt "70" ]
+    if [ "$GMX_CI_DISABLE_CUFFTMP_DECOMPOSITION_ON_INCOMPATIBLE_DEVICES" != "" ] && [ "$computeCapability" -lt "70" ]
     then
 	echo "Compute Capability is less than 7.0, so disabling GPU PME DECOMPOSITION with cuFFTMp"
 	unset GMX_GPU_PME_DECOMPOSITION
