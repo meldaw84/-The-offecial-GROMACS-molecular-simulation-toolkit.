3c0cf4c7b6fe7866216f564b85ab0b760f768c33
Berk Hess <hess@kth.se>
2022-12-15 11:53:30 +0100

    Removed unused from kernel arguments

--- src/gromacs/nbnxm/kernels_simd_2xmm/kernel_ElecQSTab_VdwLJCombLB_VF.cpp
+++ src/gromacs/nbnxm/kernels_simd_2xmm/kernel_ElecQSTab_VdwLJCombLB_VF.cpp
@@ -58,11 +58,11 @@ template void nbnxmKernelSimd<KernelLayout::r2xMM,
                               LJCombinationRule::LorentzBerthelot,
                               InteractionModifiers::PotShift,
                               false,
-                              EnergyOutput::System>(const NbnxnPairlistCpu gmx_unused* nbl,
-                                                    const nbnxn_atomdata_t gmx_unused* nbat,
-                                                    const interaction_const_t gmx_unused* ic,
-                                                    const rvec gmx_unused*  shift_vec,
-                                                    nbnxn_atomdata_output_t gmx_unused* out);
+                              EnergyOutput::System>(const NbnxnPairlistCpu*    nbl,
+                                                    const nbnxn_atomdata_t*    nbat,
+                                                    const interaction_const_t* ic,
+                                                    const rvec*                shift_vec,
+                                                    nbnxn_atomdata_output_t*   out);
 
 } // namespace gmx
 
