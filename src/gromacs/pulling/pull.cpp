UnixJunkie/gromacs

Implemented weight factor expression
To do: check performance and gradients?

src/gromacs/pulling/pull.cpp
@@ -95,6 +95,8 @@ constexpr gmx::EnumerationArray<PullGroupGeometry, bool> sc_isAngleType = {
     { false, false, false, false, false, true, true, true }
 };
 
+static const std::vector<std::string> posVelVarNames = { "x", "y", "z", "vx", "vy", "vz" };
+
 static int groupPbcFromParams(const t_pull_group& params, bool setPbcRefToPrevStepCOM)
 {
     if (params.ind.size() <= 1)
@@ -135,7 +137,9 @@ pull_group_work_t::pull_group_work_t(const t_pull_group& params,
     atomSet(atomSet),
     mwscale(0),
     wscale(0),
-    invtm(0)
+    invtm(0),
+    weightFactorExpressionParser(!params.weightFactorExpression.empty() ? params.weightFactorExpression : "",
+                                 posVelVarNames)
 {
     clear_dvec(x);
     clear_dvec(xp);
@@ -196,6 +200,10 @@ static void apply_forces_grp_part(const pull_group_work_t& pgrp,
         if (!pgrp.localWeights.empty())
         {
             wmass *= pgrp.localWeights[i];
+        }
+        if (!pgrp.localWeightFactors.empty())
+        {
+            wmass *= pgrp.localWeightFactors[i];
         }
 
         for (int d = 0; d < DIM; d++)
@@ -267,6 +275,10 @@ static void apply_forces_cyl_grp(const pull_group_work_t& pgrp,
     for (int i = 0; i < numAtomsLocal; i++)
     {
         double weight = pgrp.localWeights[i];
+        if (!pgrp.localWeightFactors.empty())
+        {
+            weight *= pgrp.localWeightFactors[i];
+        }
         if (weight == 0)
         {
             continue;
@@ -848,7 +860,7 @@ static void do_constraint(struct pull_t* pull,
     gmx_bool bConverged_all, bConverged = FALSE;
     int      niter = 0, ii, j, m, max_iter = 100;
     double   a;
-    dvec     tmp, tmp3;
+    dvec     tmp, tmp2, tmp3;
 
     snew(r_ij, pull->coord.size());
     snew(dr_tot, pull->coord.size());
@@ -1155,13 +1167,18 @@ static void do_constraint(struct pull_t* pull,
 
         /* update the atom positions */
         auto localAtomIndices = pgrp->atomSet.localIndex();
-        copy_dvec(dr, tmp);
+        copy_dvec(dr, tmp2);
         for (gmx::Index j = 0; j < localAtomIndices.ssize(); j++)
         {
             ii = localAtomIndices[j];
             if (!pgrp->localWeights.empty())
             {
-                dsvmul(pgrp->wscale * pgrp->localWeights[j], dr, tmp);
+                dsvmul(pgrp->wscale * pgrp->localWeights[j], dr, tmp2);
+            }
+            copy_dvec(tmp2, tmp);
+            if (!pgrp->localWeightFactors.empty())
+            {
+                dsvmul(pgrp->localWeightFactors[j], tmp2, tmp);
             }
             for (m = 0; m < DIM; m++)
             {
@@ -1610,6 +1627,8 @@ real pull_potential(struct pull_t*       pull,
     {
         real dVdl = 0;
 
+        pull_calc_weight_factors(pull, x, {});
+
         pull_calc_coms(cr, pull, masses, pbc, t, x, {});
 
         /* Evaluating coordinates needs to loop forward because transformation coordinates
@@ -1713,6 +1732,8 @@ void pull_constraint(struct pull_t*       pull,
 
     if (pull->comm.bParticipate)
     {
+        pull_calc_weight_factors(pull, x, v);
+
         pull_calc_coms(cr, pull, masses, pbc, t, x, xp);
 
         do_constraint(pull, pbc, xp, v, MAIN(cr), vir, dt, t);
@@ -1742,6 +1763,16 @@ void dd_make_local_pull_groups(const t_commrec* cr, struct pull_t* pull)
             for (size_t i = 0; i < group.atomSet.numAtomsLocal(); ++i)
             {
                 group.localWeights[i] = group.globalWeights[group.atomSet.collectiveIndex()[i]];
+            }
+        }
+
+        if (!group.globalWeightFactors.empty())
+        {
+            group.localWeightFactors.resize(group.atomSet.numAtomsLocal());
+            for (size_t i = 0; i < group.atomSet.numAtomsLocal(); ++i)
+            {
+                group.localWeightFactors[i] =
+                        group.globalWeightFactors[group.atomSet.collectiveIndex()[i]];
             }
         }
 
@@ -1869,11 +1900,14 @@ static void init_pull_group_index(FILE*              fplog,
      * But we still want to have the correct mass-weighted COMs.
      * So we store the real masses in the weights.
      */
-    const bool setWeights = (!pg->params.weight.empty() || EI_ENERGY_MINIMIZATION(ir->eI)
+    const bool setWeights       = (!pg->params.weight.empty() || EI_ENERGY_MINIMIZATION(ir->eI)
                              || ir->eI == IntegrationAlgorithm::BD);
+    const bool setWeightFactors = !pg->params.weightFactorExpression.empty();
 
     /* In parallel, store we need to extract localWeights from weights at DD time */
     std::vector<real>& weights = ((cr && PAR(cr)) ? pg->globalWeights : pg->localWeights);
+    std::vector<real>& weightFactors =
+            ((cr && PAR(cr)) ? pg->globalWeightFactors : pg->localWeightFactors);
 
     const SimulationGroups& groups = mtop.groups;
 
@@ -1947,6 +1981,10 @@ static void init_pull_group_index(FILE*              fplog,
         if (setWeights)
         {
             weights.push_back(w);
+        }
+        if (setWeightFactors)
+        {
+            weightFactors.push_back(1.0);
         }
         tmass += m;
         wmass += m * w;

src/gromacs/pulling/pull_internal.h
@@ -110,13 +110,16 @@ struct pull_group_work_t
     const int maxNumThreads; /**< The maximum number of threads to use for operations on x and f */
     bool      needToCalcCom; /**< Do we need to calculate the COM? (Not for group 0 or if only used as cylinder group) */
     std::vector<real> globalWeights; /**< Weights per atom set by the user and/or mass/friction coefficients, if empty all weights are equal */
+    std::vector<real> globalWeightFactors; /**< Weight factors per atom */
 
     /* Data modified only at init or at domain decomposition */
-    gmx::LocalAtomSet                  atomSet;      /**< Global to local atom set mapper */
-    std::vector<real>                  localWeights; /**< Weights for the local atoms */
-    std::unique_ptr<gmx::LocalAtomSet> pbcAtomSet;   /**< Keeps index of the pbc reference atom.
-                                                          The stored LocalAtomSet consists of exactly   one atom when pbc reference atom is required.
-                                                          When no pbc refence atom is used, this   pointer   shall be null. */
+    gmx::LocalAtomSet atomSet;            /**< Global to local atom set mapper */
+    std::vector<real> localWeights;       /**< Weights for the local atoms */
+    std::vector<real> localWeightFactors; /**< Weight factors for the local atoms */
+
+    std::unique_ptr<gmx::LocalAtomSet> pbcAtomSet; /**< Keeps index of the pbc reference atom.
+                                                        The stored LocalAtomSet consists of exactly one atom when pbc reference atom is required.
+                                                        When no pbc refence atom is used, this pointer shall be null. */
 
     /* Data, potentially, changed at every pull call */
     real mwscale; /**< mass*weight scaling factor 1/sum w m */
@@ -127,6 +130,8 @@ struct pull_group_work_t
     dvec                                  x;   /**< COM before update */
     dvec                                  xp;  /**< COM after update before constraining */
     dvec                                  x_prev_step; /**< center of mass of the previous step */
+
+    gmx::PullCoordExpressionParser weightFactorExpressionParser; /**< Weight factor expression parser */
 };
 
 /* Struct describing the instantaneous spatial layout of a pull coordinate */

src/gromacs/pulling/pullutil.cpp
@@ -498,6 +498,59 @@ static void sum_com_part_cosweight(const pull_group_work_t* pgrp,
     sum_com->sum_smp = sum_smp;
 }
 
+static real getWeightFactor(pull_group_work_t* pgrp, int g, const RVec x, const RVec v)
+{
+    real result = 0.0;
+    try
+    {
+        std::vector<double> position_velocity = { x[XX], x[YY], x[ZZ], v[XX], v[YY], v[ZZ] };
+        result = pgrp->weightFactorExpressionParser.evaluate(position_velocity);
+    }
+#if HAVE_MUPARSER
+    catch (mu::Parser::exception_type& e)
+    {
+        GMX_THROW(gmx::InconsistentInputError(gmx::formatString(
+                "failed to evaluate expression for pull-group%d: %s\n", g, e.GetMsg().c_str())));
+    }
+#endif
+    catch (std::exception& e)
+    {
+        GMX_THROW(gmx::InconsistentInputError(
+                gmx::formatString("failed to evaluate expression for pull-group%d.\n"
+                                  "Message:  %s\n",
+                                  g,
+                                  e.what())));
+    }
+    return result;
+}
+
+void pull_calc_weight_factors(struct pull_t* pull, ArrayRef<const RVec> x, ArrayRef<const RVec> v)
+{
+    for (size_t g = 0; g < pull->group.size(); g++)
+    {
+        pull_group_work_t& pgrp = pull->group[g];
+        if (!pgrp.params.weightFactorExpression.empty())
+        {
+            for (size_t i = 0; i < pgrp.atomSet.numAtomsLocal(); i++)
+            {
+                int ii = pgrp.atomSet.localIndex()[i];
+                /* Check if x coordinate of the atom is in the slice */
+                RVec atom_position = x[ii];
+                RVec atom_velocity;
+                if (!v.empty())
+                {
+                    atom_velocity = v[ii];
+                }
+                else
+                {
+                    atom_velocity = { 0., 0., 0. };
+                }
+                pgrp.localWeightFactors[i] = getWeightFactor(&pgrp, g, atom_position, atom_velocity);
+            }
+        }
+    }
+}
+
 /* calculates center of mass of selection index from all coordinates x */
 void pull_calc_coms(const t_commrec*     cr,
                     pull_t*              pull,

src/gromacs/pulling/tests/pull.cpp
@@ -54,6 +54,7 @@
 #    include <muParser.h>
 #endif
 
+#include "gromacs/domdec/localatomsetmanager.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/pbcutil/pbc.h"
 #include "gromacs/pulling/pull_internal.h"
@@ -66,6 +67,9 @@
 
 namespace gmx
 {
+
+extern template LocalAtomSet LocalAtomSetManager::add<void, void>(ArrayRef<const int> globalAtomIndex);
+
 namespace
 {
 
@@ -428,6 +432,66 @@ TEST_F(PullTest, TransformationCoordDummyExpression)
     double value = getTransformationPullCoordinateValue(
             &pull.coord[0], ArrayRef<const pull_coord_work_t>{}, 0.0);
     EXPECT_REAL_EQ_TOL(value, 10, defaultRealTolerance());
+}
+
+TEST_F(PullTest, WeightOneOrZero)
+{
+    //-----_SETUP-------
+    const RVec x[] = {
+        { 0.15, 0.0, 0.0 }, { 0.25, 0.0, 0.0 }, { 0.35, 0.0, 0.0 }, { 0.45, 0.0, 0.0 }, { 0.55, 0.0, 0.0 }
+    };
+    const int ind[] = { 1, 2, 4 };
+
+    LocalAtomSetManager atomSets;
+    pull_t              pull;
+    t_pull_group        params;
+    params.weightFactorExpression = "x >= 0.3 && x < 0.5 ? 1.0 : 0.0";
+
+    pull.group.emplace_back(params, atomSets.add(ind), false, 1);
+    pull_group_work_t& pgrp = pull.group[0];
+    pgrp.localWeightFactors.resize(pgrp.atomSet.numAtomsLocal());
+
+    //-----TESTS -------
+    pull_calc_weight_factors(&pull, x, {});
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[0], 0.0, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[1], 1.0, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[2], 0.0, defaultRealTolerance());
+}
+
+TEST_F(PullTest, WeightSumPositionOrVelocity)
+{
+    //-----_SETUP-------
+    const RVec x[] = {
+        { 1.1, 1.2, 1.3 }, { 2.1, 2.2, 2.3 }, { 3.1, 3.2, 3.3 }, { 4.1, 4.2, 4.3 }, { 5.1, 5.2, 5.3 }
+    };
+    const RVec v[]   = { { 0.11, 0.12, 0.13 },
+                       { 0.21, 0.22, 0.23 },
+                       { 0.31, 0.32, 0.33 },
+                       { 0.41, 0.42, 0.43 },
+                       { 0.51, 0.52, 0.53 } };
+    const int  ind[] = { 1, 2, 4 };
+
+    LocalAtomSetManager atomSets;
+    pull_t              pull;
+    t_pull_group        params0, params1;
+    params0.weightFactorExpression = "x + y + z";
+    params1.weightFactorExpression = "vx + vy + vz";
+
+    pull.group.emplace_back(params0, atomSets.add(ind), false, 1);
+    pull.group.emplace_back(params1, atomSets.add(ind), false, 1);
+    pull_group_work_t& pgrp0 = pull.group[0];
+    pull_group_work_t& pgrp1 = pull.group[1];
+    pgrp0.localWeightFactors.resize(pgrp0.atomSet.numAtomsLocal());
+    pgrp1.localWeightFactors.resize(pgrp1.atomSet.numAtomsLocal());
+
+    //-----TESTS -------
+    pull_calc_weight_factors(&pull, x, v);
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[0], 6.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[1], 9.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[2], 15.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[0], 0.66, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[1], 0.96, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[2], 1.56, defaultRealTolerance());
 }
 #endif // HAVE_MUPARSER
 

meldaw84/-The-offecial-GROMACS-molecular-simulation-toolkit.

src/gromacs/pulling/pull.cpp
@@ -95,6 +95,8 @@ constexpr gmx::EnumerationArray<PullGroupGeometry, bool> sc_isAngleType = {
     { false, false, false, false, false, true, true, true }
 };
 
+static const std::vector<std::string> posVelVarNames = { "x", "y", "z", "vx", "vy", "vz" };
+
 static int groupPbcFromParams(const t_pull_group& params, bool setPbcRefToPrevStepCOM)
 {
     if (params.ind.size() <= 1)
@@ -135,7 +137,9 @@ pull_group_work_t::pull_group_work_t(const t_pull_group& params,
     atomSet_(atomSet),
     mwscale(0),
     wscale(0),
-    invtm(0)
+    invtm(0),
+    weightFactorExpressionParser(!params.weightFactorExpression.empty() ? params.weightFactorExpression : "",
+                                 posVelVarNames)
 {
     clear_dvec(x);
     clear_dvec(xp);
@@ -196,6 +200,10 @@ static void apply_forces_grp_part(const pull_group_work_t& pgrp,
         if (!pgrp.localWeights.empty())
         {
             wmass *= pgrp.localWeights[i];
+        }
+        if (!pgrp.localWeightFactors.empty())
+        {
+            wmass *= pgrp.localWeightFactors[i];
         }
 
         for (int d = 0; d < DIM; d++)
@@ -267,6 +275,10 @@ static void apply_forces_cyl_grp(const pull_group_work_t& pgrp,
     for (int i = 0; i < numAtomsLocal; i++)
     {
         double weight = pgrp.localWeights[i];
+        if (!pgrp.localWeightFactors.empty())
+        {
+            weight *= pgrp.localWeightFactors[i];
+        }
         if (weight == 0)
         {
             continue;
@@ -848,7 +860,7 @@ static void do_constraint(struct pull_t* pull,
     gmx_bool bConverged_all, bConverged = FALSE;
     int      niter = 0, ii, j, m, max_iter = 100;
     double   a;
-    dvec     tmp, tmp3;
+    dvec     tmp, tmp2, tmp3;
 
     snew(r_ij, pull->coord.size());
     snew(dr_tot, pull->coord.size());
@@ -1155,13 +1167,18 @@ static void do_constraint(struct pull_t* pull,
 
         /* update the atom positions */
         auto localAtomIndices = pgrp->atomSet_.localIndex();
-        copy_dvec(dr, tmp);
+        copy_dvec(dr, tmp2);
         for (gmx::Index j = 0; j < localAtomIndices.ssize(); j++)
         {
             ii = localAtomIndices[j];
             if (!pgrp->localWeights.empty())
             {
-                dsvmul(pgrp->wscale * pgrp->localWeights[j], dr, tmp);
+                dsvmul(pgrp->wscale * pgrp->localWeights[j], dr, tmp2);
+            }
+            copy_dvec(tmp2, tmp);
+            if (!pgrp->localWeightFactors.empty())
+            {
+                dsvmul(pgrp->localWeightFactors[j], tmp2, tmp);
             }
             for (m = 0; m < DIM; m++)
             {
@@ -1610,6 +1627,8 @@ real pull_potential(struct pull_t*       pull,
     {
         real dVdl = 0;
 
+        pull_calc_weight_factors(pull, x, {});
+
         pull_calc_coms(cr, pull, masses, pbc, t, x, {});
 
         /* Evaluating coordinates needs to loop forward because transformation coordinates
@@ -1713,6 +1732,8 @@ void pull_constraint(struct pull_t*       pull,
 
     if (pull->comm.bParticipate)
     {
+        pull_calc_weight_factors(pull, x, v);
+
         pull_calc_coms(cr, pull, masses, pbc, t, x, xp);
 
         do_constraint(pull, pbc, xp, v, MAIN(cr), vir, dt, t);
@@ -1742,6 +1763,16 @@ void dd_make_local_pull_groups(const t_commrec* cr, struct pull_t* pull)
             for (size_t i = 0; i < group.atomSet_.numAtomsLocal(); ++i)
             {
                 group.localWeights[i] = group.globalWeights[group.atomSet_.collectiveIndex()[i]];
+            }
+        }
+
+        if (!group.globalWeightFactors.empty())
+        {
+            group.localWeightFactors.resize(group.atomSet_.numAtomsLocal());
+            for (size_t i = 0; i < group.atomSet_.numAtomsLocal(); ++i)
+            {
+                group.localWeightFactors[i] =
+                        group.globalWeightFactors[group.atomSet_.collectiveIndex()[i]];
             }
         }
 
@@ -1869,11 +1900,14 @@ static void init_pull_group_index(FILE*              fplog,
      * But we still want to have the correct mass-weighted COMs.
      * So we store the real masses in the weights.
      */
-    const bool setWeights = (!pg->params_.weight.empty() || EI_ENERGY_MINIMIZATION(ir->eI)
+    const bool setWeights       = (!pg->params_.weight.empty() || EI_ENERGY_MINIMIZATION(ir->eI)
                              || ir->eI == IntegrationAlgorithm::BD);
+    const bool setWeightFactors = !pg->params_.weightFactorExpression.empty();
 
     /* In parallel, store we need to extract localWeights from weights at DD time */
     std::vector<real>& weights = ((cr && PAR(cr)) ? pg->globalWeights : pg->localWeights);
+    std::vector<real>& weightFactors =
+            ((cr && PAR(cr)) ? pg->globalWeightFactors : pg->localWeightFactors);
 
     const SimulationGroups& groups = mtop.groups;
 
@@ -1947,6 +1981,10 @@ static void init_pull_group_index(FILE*              fplog,
         if (setWeights)
         {
             weights.push_back(w);
+        }
+        if (setWeightFactors)
+        {
+            weightFactors.push_back(1.0);
         }
         tmass += m;
         wmass += m * w;

src/gromacs/pulling/pull_internal.h
@@ -110,13 +110,15 @@ struct pull_group_work_t
     const int maxNumThreads_; /**< The maximum number of threads to use for operations on x and f */
     bool      needToCalcCom; /**< Do we need to calculate the COM? (Not for group 0 or if only used as cylinder group) */
     std::vector<real> globalWeights; /**< Weights per atom set by the user and/or mass/friction coefficients, if empty all weights are equal */
+    std::vector<real> globalWeightFactors; /**< Weight factors per atom */
 
     /* Data modified only at init or at domain decomposition */
-    gmx::LocalAtomSet                  atomSet_;     /**< Global to local atom set mapper */
-    std::vector<real>                  localWeights; /**< Weights for the local atoms */
-    std::unique_ptr<gmx::LocalAtomSet> pbcAtomSet;   /**< Keeps index of the pbc reference atom.
-                                                          The stored LocalAtomSet consists of exactly   one atom when pbc reference atom is required.
-                                                          When no pbc refence atom is used, this   pointer   shall be null. */
+    gmx::LocalAtomSet atomSet_;                    /**< Global to local atom set mapper */
+    std::vector<real> localWeights;                /**< Weights for the local atoms */
+    std::vector<real> localWeightFactors;          /**< Weight factors for the local atoms */
+    std::unique_ptr<gmx::LocalAtomSet> pbcAtomSet; /**< Keeps index of the pbc reference atom.
+                                                        The stored LocalAtomSet consists of exactly one atom when pbc reference atom is required.
+                                                        When no pbc refence atom is used, this pointer shall be null. */
 
     /* Data, potentially, changed at every pull call */
     real mwscale; /**< mass*weight scaling factor 1/sum w m */
@@ -127,6 +129,8 @@ struct pull_group_work_t
     dvec                                  x;   /**< COM before update */
     dvec                                  xp;  /**< COM after update before constraining */
     dvec                                  x_prev_step; /**< center of mass of the previous step */
+
+    gmx::PullCoordExpressionParser weightFactorExpressionParser; /**< Weight factor expression parser */
 };
 
 /* Struct describing the instantaneous spatial layout of a pull coordinate */

src/gromacs/pulling/pullutil.cpp
@@ -498,6 +498,59 @@ static void sum_com_part_cosweight(const pull_group_work_t* pgrp,
     sum_com->sum_smp = sum_smp;
 }
 
+static real getWeightFactor(pull_group_work_t* pgrp, int g, const RVec x, const RVec v)
+{
+    real result = 0.0;
+    try
+    {
+        std::vector<double> position_velocity = { x[XX], x[YY], x[ZZ], v[XX], v[YY], v[ZZ] };
+        result = pgrp->weightFactorExpressionParser.evaluate(position_velocity);
+    }
+#if HAVE_MUPARSER
+    catch (mu::Parser::exception_type& e)
+    {
+        GMX_THROW(gmx::InconsistentInputError(gmx::formatString(
+                "failed to evaluate expression for pull-group%d: %s\n", g, e.GetMsg().c_str())));
+    }
+#endif
+    catch (std::exception& e)
+    {
+        GMX_THROW(gmx::InconsistentInputError(
+                gmx::formatString("failed to evaluate expression for pull-group%d.\n"
+                                  "Message:  %s\n",
+                                  g,
+                                  e.what())));
+    }
+    return result;
+}
+
+void pull_calc_weight_factors(struct pull_t* pull, ArrayRef<const RVec> x, ArrayRef<const RVec> v)
+{
+    for (size_t g = 0; g < pull->group.size(); g++)
+    {
+        pull_group_work_t& pgrp = pull->group[g];
+        if (!pgrp.params_.weightFactorExpression.empty())
+        {
+            for (size_t i = 0; i < pgrp.atomSet_.numAtomsLocal(); i++)
+            {
+                int ii = pgrp.atomSet_.localIndex()[i];
+                /* Check if x coordinate of the atom is in the slice */
+                RVec atom_position = x[ii];
+                RVec atom_velocity;
+                if (!v.empty())
+                {
+                    atom_velocity = v[ii];
+                }
+                else
+                {
+                    atom_velocity = { 0., 0., 0. };
+                }
+                pgrp.localWeightFactors[i] = getWeightFactor(&pgrp, g, atom_position, atom_velocity);
+            }
+        }
+    }
+}
+
 /* calculates center of mass of selection index from all coordinates x */
 void pull_calc_coms(const t_commrec*     cr,
                     pull_t*              pull,

src/gromacs/pulling/tests/pull.cpp
@@ -54,6 +54,7 @@
 #    include <muParser.h>
 #endif
 
+#include "gromacs/domdec/localatomsetmanager.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/pbcutil/pbc.h"
 #include "gromacs/pulling/pull_internal.h"
@@ -66,6 +67,9 @@
 
 namespace gmx
 {
+
+extern template LocalAtomSet LocalAtomSetManager::add<void, void>(ArrayRef<const int> globalAtomIndex);
+
 namespace
 {
 
@@ -428,6 +432,66 @@ TEST_F(PullTest, TransformationCoordDummyExpression)
     double value = getTransformationPullCoordinateValue(
             &pull.coord[0], ArrayRef<const pull_coord_work_t>{}, 0.0);
     EXPECT_REAL_EQ_TOL(value, 10, defaultRealTolerance());
+}
+
+TEST_F(PullTest, WeightOneOrZero)
+{
+    //-----_SETUP-------
+    const RVec x[] = {
+        { 0.15, 0.0, 0.0 }, { 0.25, 0.0, 0.0 }, { 0.35, 0.0, 0.0 }, { 0.45, 0.0, 0.0 }, { 0.55, 0.0, 0.0 }
+    };
+    const int ind[] = { 1, 2, 4 };
+
+    LocalAtomSetManager atomSets;
+    pull_t              pull;
+    t_pull_group        params;
+    params.weightFactorExpression = "x >= 0.3 && x < 0.5 ? 1.0 : 0.0";
+
+    pull.group.emplace_back(params, atomSets.add(ind), false, 1);
+    pull_group_work_t& pgrp = pull.group[0];
+    pgrp.localWeightFactors.resize(pgrp.atomSet_.numAtomsLocal());
+
+    //-----TESTS -------
+    pull_calc_weight_factors(&pull, x, {});
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[0], 0.0, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[1], 1.0, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp.localWeightFactors[2], 0.0, defaultRealTolerance());
+}
+
+TEST_F(PullTest, WeightSumPositionOrVelocity)
+{
+    //-----_SETUP-------
+    const RVec x[] = {
+        { 1.1, 1.2, 1.3 }, { 2.1, 2.2, 2.3 }, { 3.1, 3.2, 3.3 }, { 4.1, 4.2, 4.3 }, { 5.1, 5.2, 5.3 }
+    };
+    const RVec v[]   = { { 0.11, 0.12, 0.13 },
+                       { 0.21, 0.22, 0.23 },
+                       { 0.31, 0.32, 0.33 },
+                       { 0.41, 0.42, 0.43 },
+                       { 0.51, 0.52, 0.53 } };
+    const int  ind[] = { 1, 2, 4 };
+
+    LocalAtomSetManager atomSets;
+    pull_t              pull;
+    t_pull_group        params0, params1;
+    params0.weightFactorExpression = "x + y + z";
+    params1.weightFactorExpression = "vx + vy + vz";
+
+    pull.group.emplace_back(params0, atomSets.add(ind), false, 1);
+    pull.group.emplace_back(params1, atomSets.add(ind), false, 1);
+    pull_group_work_t& pgrp0 = pull.group[0];
+    pull_group_work_t& pgrp1 = pull.group[1];
+    pgrp0.localWeightFactors.resize(pgrp0.atomSet_.numAtomsLocal());
+    pgrp1.localWeightFactors.resize(pgrp1.atomSet_.numAtomsLocal());
+
+    //-----TESTS -------
+    pull_calc_weight_factors(&pull, x, v);
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[0], 6.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[1], 9.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp0.localWeightFactors[2], 15.6, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[0], 0.66, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[1], 0.96, defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(pgrp1.localWeightFactors[2], 1.56, defaultRealTolerance());
 }
 #endif // HAVE_MUPARSER
 