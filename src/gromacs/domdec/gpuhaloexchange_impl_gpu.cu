3372fc4270a272adadd5157b6de6e9037f01a4be
Mark Abraham <mark.j.abraham@gmail.com>
2023-08-13 21:28:24 +0200

    clang format

--- src/gromacs/domdec/gpuhaloexchange_impl_gpu.cu
+++ src/gromacs/domdec/gpuhaloexchange_impl_gpu.cu
@@ -46,9 +46,9 @@
 
 #include "config.h"
 
+#include "gromacs/gpu_utils/gpuhaloexchange.h"
 #include "gromacs/gpu_utils/typecasts.cuh"
 #include "gromacs/gpu_utils/vectype_ops.cuh"
-#include "gromacs/gpu_utils/gpuhaloexchange.h"
 
 #include "domdec_struct.h"
 
