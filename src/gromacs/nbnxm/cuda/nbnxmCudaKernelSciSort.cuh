6acc8ec6b2561b703e1b7436c0738679f801bbb5
Ania Brown <anbrown@nvidia.com>
2023-10-12 06:03:17 -0700

    adding comments for gpu sort algorithm

--- src/gromacs/nbnxm/cuda/nbnxmCudaKernelSciSort.cuh
+++ src/gromacs/nbnxm/cuda/nbnxmCudaKernelSciSort.cuh
@@ -44,10 +44,19 @@
 
 /*! \brief CUDA bucket sci sort kernel.
  *
- *  Unlike the cpu version of sci sort, this kernel uses counts which only contain atoms which have
- *  not been masked out, giving an order which more accurately represents the work which will be
- * done in the non bonded force kernel. The counts themselves are generated in the prune kernel.
+ *  Sorts sci in order from most to least neighbours, using the count sort algorithm
  *
+ *  Unlike the cpu version of sci sort, this kernel uses counts which only contain pairs which have
+ *  not been masked out, giving an ordering which more accurately represents the work which will be
+ *  done in the non bonded force kernel. The counts themselves are generated in the prune kernel.
+ *
+ *  Inputs:
+ *   - plist.sci = unsorted pair list
+ *   - plist.sorting.sciCount[i] = total number of sci with exactly i neighbours
+ *   - plist.sorting.sciOffset = exclusive prefix sum of sciCount.
+ *     plist.sorting.sciOffset[i] represents the offset that the first sci with i neighbours will
+ *     have in the sorted sci list. All other sci with i neighbours will be placed randomly in
+ * positions plist.sorting.sciOffset[i] to plist.sorting.sciOffset[i+1] exclusive
  */
 
 #ifndef NBNXM_CUDA_KERNEL_SCI_SORT_CUH
@@ -75,6 +84,7 @@ __launch_bounds__(c_sciSortingThreadsPerBlock) __global__
     {
         if (size > (block_offset + c_sciSortingItemsPerThread * flat_id + i))
         {
+            // Copy plist to intermediate registers for clarity
             sci[i]       = pl_sci[block_offset + c_sciSortingItemsPerThread * flat_id + i];
             sci_count[i] = pl_sci_count[block_offset + c_sciSortingItemsPerThread * flat_id + i];
         }
@@ -85,7 +95,12 @@ __launch_bounds__(c_sciSortingThreadsPerBlock) __global__
     {
         if (size > (block_offset + c_sciSortingItemsPerThread * flat_id + i))
         {
-            sci_offset[i]              = atomicAdd(&pl_sci_offset[sci_count[i]], 1);
+            // Choose an index for each sci with exactly sci_count[i] neighbours, using
+            // offsets from pl_sci_offset[count] to pl_sci_offset[count+1] exclusive.
+            // As the amount of work for each of these in the non bonded force kernel will
+            // be equivalent, we don't care what order these are placed in.
+            sci_offset[i] = atomicAdd(&pl_sci_offset[sci_count[i]], 1);
+            // Insert the sci into the sorted list at the chosen index
             pl_sci_sort[sci_offset[i]] = sci[i];
         }
     }
